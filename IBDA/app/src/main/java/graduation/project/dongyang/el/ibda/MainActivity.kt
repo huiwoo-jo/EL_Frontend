a32d6901dca2dad6a7c17aa3b32a450fff7cc5fd
조희우 <84004687+helena0589@users.noreply.github.com>
2023-04-12 00:34:14 +0900

    Feat : Scan QR

--- IBDA/app/src/main/java/graduation/project/dongyang/el/ibda/MainActivity.kt
+++ IBDA/app/src/main/java/graduation/project/dongyang/el/ibda/MainActivity.kt
@@ -1,14 +1,52 @@
 package graduation.project.dongyang.el.ibda
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 import kr.project.dongyang.EL.IBDA.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        // result : 스캔된 결과
+        // 내용이 없다면
+        if (result.contents == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        }
+        else { // 내용이 있다면
+
+            // 1. Toast 메시지 출력.
+            Toast.makeText(
+                this,
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+
+            // 2. 결과 값 TextView에 출력
+            binding.qrResult.text="Scan Result : \n" + result.contents.toString()
+
+            binding.qrResult.setOnClickListener {
+                val address = result.contents.toString()
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun onScanButtonClicked(){
+        barcodeLauncher.launch(ScanOptions())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -21,9 +59,9 @@ class MainActivity : AppCompatActivity() {
             val intent=Intent(this, CreateQR::class.java)
             startActivity(intent)
         }
-        binding.readQR.setOnClickListener{
-            val intent=Intent(this, ReadQR::class.java)
-            startActivity(intent)
+        binding.readQR.setOnClickListener {
+            onScanButtonClicked()
         }
+
     }
 }

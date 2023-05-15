1cc4239543faa00bf8b23e21124cb7c7545a15c2
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-05-15 15:18:26 +0900

    Feat : Login

--- IBDA/app/src/main/java/graduation/project/dongyang/el/ibda/LoginActivity.kt
+++ IBDA/app/src/main/java/graduation/project/dongyang/el/ibda/LoginActivity.kt
@@ -1,5 +1,6 @@
 package graduation.project.dongyang.el.ibda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.project.dongyang.EL.IBDA.databinding.ActivityLoginBinding
@@ -13,6 +14,30 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        //button click
+        binding.btnLogin.setOnClickListener{
+            //get values from input field
+            val userId = binding.inputId.toString().trim()
+            val userPwd = binding.inputPwd.toString().trim()
+
+            //empty error
+            if(userId.isEmpty()) //return true if Id is empty
+            {
+                binding.inputId.error = "Enter ID"
+                binding.inputId.requestFocus()
+            }else if (userPwd.isEmpty()) //return true Pwd is empty
+            {
+                binding.inputPwd.error= "Enter Pwd"
+                binding.inputPwd.requestFocus()
+            }else{ //validation is successful
+                //open clothes activity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
     //end : OnCreate
 }

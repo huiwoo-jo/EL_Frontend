d5cd3e5178774aa8c1d59c7a01db93b49a59d125
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-05-24 12:49:56 +0900

    Fix : 호출 충돌 오류

--- IBDA/app/src/main/java/graduation/project/dongyang/el/ibda/MainClothes.kt
+++ IBDA/app/src/main/java/graduation/project/dongyang/el/ibda/MainClothes.kt
@@ -5,16 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-
 import kr.project.dongyang.EL.IBDA.R
 import kr.project.dongyang.EL.IBDA.databinding.ActivityMainClothesBinding
 import org.json.JSONArray

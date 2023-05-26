7255b61481684f1d190df2de06c561244430793a
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-05-26 10:33:34 +0900

    Admin - Feat : Login

--- IBDA_admin/app/src/main/java/kr/project/dongyang/el/ibda/ibda_admin/LoginActivity.kt
+++ IBDA_admin/app/src/main/java/kr/project/dongyang/el/ibda/ibda_admin/LoginActivity.kt
@@ -1,16 +1,13 @@
 package kr.project.dongyang.el.ibda.ibda_admin
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import kr.project.dongyang.el.ibda.ibda_admin.databinding.ActivityLoginBinding
 import kr.project.dongyang.el.ibda.ibda_admin.databinding.ActivityMainBinding
 
-
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityLoginBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
